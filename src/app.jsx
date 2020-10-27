5aadaa3f6352bb16560877b217f0e18ca6e05993
sookyung93 <i.sookyung93@gmail.com>
2020-10-27 17:04:56 +0900

    make video list & add css

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './app.css';
+import Header from './components/header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
@@ -21,7 +22,11 @@ function App() {
       .catch((error) => console.log('error', error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <>
+      <VideoList videos={videos} />
+    </>
+  );
 }
 
 export default App;
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => {
-  return <p>{props.video.snippet.title}</p>;
+const VideoItem = ({ video }) => {
+  return (
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={video.snippet.thumbnails.medium.url}
+          alt=""
+        />
+        <div className={styles.description}>
+          <p className={styles.description__title}>{video.snippet.title}</p>
+          <p className={styles.description__channelTitle}>
+            {video.snippet.channelTitle}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoItem;
