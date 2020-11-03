6d2e79bb66d3001805d3091ab0233915568f19fc
sookyung93 <i.sookyung93@gmail.com>
2020-11-03 16:49:08 +0900

    add responsive web design

--- src/components/video_play/video_play.jsx
+++ src/components/video_play/video_play.jsx
@@ -5,9 +5,9 @@ const VideoPlay = (props) => {
   return (
     <div className={styles.container}>
       <iframe
+        className={styles.player}
         title="video-paly"
         width="100%"
-        height="500"
         src={`https://www.youtube.com/embed/${props.video.id}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
