928800ee5791983d38ab41587325e1154495e4bf
sookyung93 <i.sookyung93@gmail.com>
2020-11-03 16:57:54 +0900

    Updates

---
+++ precache-manifest.dbf80387a0ee78a405a1e6f14a4fa99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b53c1013fd3b06eaaf53a68e48cdcf",
+    "url": "/youtube_react/index.html"
+  },
+  {
+    "revision": "9cbc1d9782accb0e0ca4",
+    "url": "/youtube_react/static/css/main.87bde85b.chunk.css"
+  },
+  {
+    "revision": "591f994170fa35bc3227",
+    "url": "/youtube_react/static/js/2.2f4b5090.chunk.js"
+  },
+  {
+    "revision": "76aa3a6712dcb2b7248a77b49e0cd963",
+    "url": "/youtube_react/static/js/2.2f4b5090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbc1d9782accb0e0ca4",
+    "url": "/youtube_react/static/js/main.64eb99c0.chunk.js"
+  },
+  {
+    "revision": "3cd4bfbdb1f4c828dc32",
+    "url": "/youtube_react/static/js/runtime-main.02c8cee0.js"
+  }
+]);
